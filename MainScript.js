6957b5a4aab7af75f87a5df183180f690d1417ae
AmirMS90 <amir.m.s1010@gmail.com>
2024-05-11 16:33:09 +0330

    Added disable tag when menue closed ❌

--- MainScript.js
+++ MainScript.js
@@ -3,11 +3,13 @@ const HamMenueXButton = document.getElementById('ham_x');
 const HamMenue = document.querySelector('.menue');
 
 HamMenueButton.addEventListener('click', (e) => {
+    HamMenue.classList.remove('disabled');
     HamMenue.classList.add('enabled');
     e.preventDefault();
 });
 
 HamMenueXButton.addEventListener('click', (e) => {
     HamMenue.classList.remove('enabled');
+    HamMenue.classList.add('disabled');
     e.preventDefault();
 });
